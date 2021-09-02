ad3dcc7a247ab5c977e3222f195224bce77974eb
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-02 16:59:59 -0300

    Criar classe cliente controller

---
+++ src/main/java/br/com/zup/lidiane/investimento/cliente/ClienteController.java
@@ -0,0 +1,13 @@
+package br.com.zup.lidiane.investimento.cliente;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+}
