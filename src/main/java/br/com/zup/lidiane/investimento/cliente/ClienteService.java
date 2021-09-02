550a896b7ee8cd0164542933c3a8d2ffff4bdd7e
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-02 16:26:20 -0300

    Criar classe service de cliente

---
+++ src/main/java/br/com/zup/lidiane/investimento/cliente/ClienteService.java
@@ -0,0 +1,11 @@
+package br.com.zup.lidiane.investimento.cliente;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+}
