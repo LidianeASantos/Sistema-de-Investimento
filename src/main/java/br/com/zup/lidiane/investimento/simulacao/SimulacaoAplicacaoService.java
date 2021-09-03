e29907adccb672c93b017973f851320e7b3c4c12
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-03 09:14:50 -0300

    Calcular data de resgate

--- src/main/java/br/com/zup/lidiane/investimento/simulacao/SimulacaoAplicacaoService.java
+++ src/main/java/br/com/zup/lidiane/investimento/simulacao/SimulacaoAplicacaoService.java
@@ -2,6 +2,8 @@ package br.com.zup.lidiane.investimento.simulacao;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class SimulacaoAplicacaoService {
 
@@ -9,5 +11,9 @@ public class SimulacaoAplicacaoService {
         double valorRendaFixa = (valorAplicado * ((0.5/100) * qtdMeses));
 
         return valorRendaFixa;
+    }
+
+    public LocalDate calcularDataResgate(int qtdMeses){
+        return LocalDate.now().plusMonths( qtdMeses );
     }
 }
