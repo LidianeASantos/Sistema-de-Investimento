2d72e3424a9832462ccc66dd3a9989f0b41570a1
Lidiane Alves dos Santos <lidiane.santos@zup.com.br>
2021-09-03 14:59:35 -0300

    Aruumar, princípio de responsabilidade

--- src/main/java/br/com/zup/lidiane/investimento/simulacao/SimulacaoAplicacaoService.java
+++ src/main/java/br/com/zup/lidiane/investimento/simulacao/SimulacaoAplicacaoService.java
@@ -1,6 +1,6 @@
 package br.com.zup.lidiane.investimento.simulacao;
 
-import br.com.zup.lidiane.investimento.dtos.ClienteSimulacaoDto;
+import br.com.zup.lidiane.investimento.cliente.Cliente;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 @Service
 public class SimulacaoAplicacaoService {
 
-    public double calcularRendaFixa(ClienteSimulacaoDto valorAplicado, int qtdMeses){
+    public double calcularRendaFixa(Cliente valorAplicado, int qtdMeses){
         double valorRendaFixa = (valorAplicado * ((0.5/100) * qtdMeses));
 
         return valorRendaFixa;
